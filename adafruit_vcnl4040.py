3b6dd04a20f005867dd05286aa80c2cae0a176d0
Kattni Rembor <kattni@adafruit.com>
2019-02-11 17:46:33 -0500

    Added docstring info.

--- adafruit_vcnl4040.py
+++ adafruit_vcnl4040.py
@@ -58,19 +58,22 @@ __repo__ = "https://github.com/adafruit/Adafruit_CircuitPython_VCNL4040.git"
 class VCNL4040:
     # ID_LM - Device ID, address
     _device_id = UnaryStruct(0x0C, "<H")
-    """Docs"""
+    """The device ID"""
 
     # PS_Data_LM - PS output data
     proximity = ROUnaryStruct(0x08, "<H")
+    """The proximity data"""
 
     # PS_CONF1 - PS duty ratio, integration time, persistence, enable/disable
     # PS_CONF2 - PS output resolution selection, interrupt trigger method
     # PS_CONF3 - PS smart persistence, active force mode
-    # proximity_settings = UnaryStruct(0x03, "<H")
     proximity_shutdown = RWBit(0x03, 0, register_width=2)
+    """Proximity sensor shutdown. When True, proximity data is disabled."""
     proximity_persistence = RWBits(3, 0x03, 1, register_width=2)
     proximity_smart_persistence = RWBit(0x04, 4, register_width=2)
     proximity_interrupt = RWBits(2, 0x03, 8, register_width=2)
+    """Interrupt enable. (0:0) disable. (0:1) trigger when close, 
+    (1:0) trigger when away, (1:1) trigger when close or away"""
     proximity_cancellation_level = UnaryStruct(0x05, "<H")
 
     # PS_THDL_LM - PS low interrupt threshold setting
@@ -79,27 +82,35 @@ class VCNL4040:
     proximity_high_threshold = UnaryStruct(0x07, "<H")
     # INT_FLAG - PS interrupt flag
     proximity_high_interrupt = ROBit(0x0B, 9, register_width=2)
+    """If close: proximity rises above high threshold interrupt trigger event"""
     proximity_low_interrupt = ROBit(0x0B, 8, register_width=2)
+    """If away: proximity drops below low threshold trigger event"""
 
     # ALS_Data_LM - ALS output data
     light = ROUnaryStruct(0x09, "<H")
+    """Ambient light data"""
 
     # ALS_CONF - ALS integration time, persistence, interrupt, function enable/disable
     light_settings = UnaryStruct(0x00, "<H")
     light_shutdown = RWBit(0x00, 0, register_width=2)
+    """Ambient light sensor shutdown. When True, ambient light data is disabled."""
 
     # ALS_THDL_LM - ALS low interrupt threshold setting
-    light_low_threshod = UnaryStruct(0x02, "<H")
+    light_low_threshold = UnaryStruct(0x02, "<H")
     # ALS_THDH_LM - ALS high interrupt threshold setting
     light_high_threshold = UnaryStruct(0x01, "<H")
     # INT_FLAG - ALS interrupt flag
     light_high_interrupt = ROBit(0x0B, 12, register_width=2)
+    """If high: ambient light sensor crosses high interrupt threshold trigger event"""
     light_low_interrupt = ROBit(0x0B, 13, register_width=2)
+    """If low: ambient light sensor crosses low interrupt threshold trigger event"""
 
     # White_Data_LM - White output data
     white = ROUnaryStruct(0x0A, "<H")
+    """White light data"""
 
     # PS_MS - White channel enable/disable, PS mode, PS protection setting, LED current
+    # White_EN - PS_MS_H, 7th bit - White channel enable/disable
     # white_enable = RWBit(0x04, 15, register_width=2)
 
     def __init__(self, i2c, address=0x60, interrupt_pin=None):
@@ -113,5 +124,5 @@ class VCNL4040:
 
         self.proximity_shutdown = False
         self.light_shutdown = False
-        self.proximity_smart_persistence = True
+        # self.proximity_smart_persistence = True
         # self.white_enable = False
