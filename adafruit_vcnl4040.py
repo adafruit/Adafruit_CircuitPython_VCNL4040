ea0898eb7d7fd81f78c4e9235f99a98eafb7bb6b
Kattni Rembor <kattni@adafruit.com>
2019-02-14 18:38:03 -0500

    Add features.

--- adafruit_vcnl4040.py
+++ adafruit_vcnl4040.py
@@ -45,6 +45,7 @@ Implementation Notes
  * Adafruit's Register library: https://github.com/adafruit/Adafruit_CircuitPython_Register
 """
 
+from micropython import const
 import adafruit_bus_device.i2c_device as i2c_device
 from adafruit_register.i2c_struct import UnaryStruct, ROUnaryStruct
 from adafruit_register.i2c_bits import RWBits, ROBits
@@ -56,6 +57,44 @@ __repo__ = "https://github.com/adafruit/Adafruit_CircuitPython_VCNL4040.git"
 
 
 class VCNL4040:
+    # Ambient light sensor integration times
+    ALS_80MS = const(0x0)
+    ALS_160MS = const(0x1)
+    ALS_320MS = const(0x2)
+    ALS_640MS = const(0x3)
+
+    # Proximity sensor integration times
+    PS_1T = const(0x0)
+    PS_1_5T = const(0x1)
+    PS_2T = const(0x2)
+    PS_2_5T = const(0x3)
+    PS_3T = const(0x4)
+    PS_3_5T = const(0x5)
+    PS_4T = const(0x6)
+    PS_8T = const(0x7)
+
+    # LED current settings
+    LED_50MA = const(0x0)
+    LED_75MA = const(0x1)
+    LED_100MA = const(0x2)
+    LED_120MA = const(0x3)
+    LED_140MA = const(0x4)
+    LED_160MA = const(0x5)
+    LED_180MA = const(0x6)
+    LED_200MA = const(0x7)
+
+    # LED duty cycle settings
+    LED_1_40 = const(0x0)
+    LED_1_80 = const(0x1)
+    LED_1_160 = const(0x2)
+    LED_1_320 = const(0x3)
+
+    # Proximity sensor enable/disable options
+    PS_INT_DISABLE = const(0x0)
+    PS_INT_CLOSE = const(0x1)
+    PS_INT_AWAY = const(0x2)
+    PS_INT_CLOSE_AWAY = const(0x3)
+
     # ID_LM - Device ID, address
     _device_id = UnaryStruct(0x0C, "<H")
     """The device ID"""
@@ -69,12 +108,22 @@ class VCNL4040:
     # PS_CONF3 - PS smart persistence, active force mode
     proximity_shutdown = RWBit(0x03, 0, register_width=2)
     """Proximity sensor shutdown. When True, proximity data is disabled."""
-    proximity_persistence = RWBits(3, 0x03, 1, register_width=2)
+    proximity_integration_time = RWBits(3, 0x03, 1, register_width=2)
+    """Proximity sensor integration time setting."""
+    proximity_persistence = RWBits(2, 0x03, 4, register_width=2)
+    """Proximity sensor interrupt persistence setting"""
     proximity_smart_persistence = RWBit(0x04, 4, register_width=2)
+    """Proximity sensor smart persistence. 0 when disabled, 1 when enabled."""
     proximity_interrupt = RWBits(2, 0x03, 8, register_width=2)
-    """Interrupt enable. (0:0) disable. (0:1) trigger when close, 
-    (1:0) trigger when away, (1:1) trigger when close or away"""
+    """Interrupt enable. Options are: (above)"""
     proximity_cancellation_level = UnaryStruct(0x05, "<H")
+    proximity_bits = RWBit(0x03, 11, register_width=2)
+    """0 when proximity sensor output is 12 bits, 1 when proximity sensor output is 16 bits."""
+    proximity_force = RWBit(0x04, 3, register_width=2)
+    """0 when active force mode is disabled (default), 1 when active force mode is enabled."""
+    proximity_force_trigger = RWBit(0x04, 2, register_width=2)
+    """0 for no proximity active force mode trigger, 1 for trigger one time cycle. Output one cycle
+    data every time host writes ``1`` to sensor. State returns to 0 automatically."""
 
     # PS_THDL_LM - PS low interrupt threshold setting
     proximity_low_threshold = UnaryStruct(0x06, "<H")
@@ -86,14 +135,22 @@ class VCNL4040:
     proximity_low_interrupt = ROBit(0x0B, 8, register_width=2)
     """If away: proximity drops below low threshold trigger event"""
 
+    led_current = RWBits(3, 0x04, 8, register_width=2)
+    """LED current selection setting"""
+    led_duty_cycle = RWBits(2, 0x03, 6, register_width=2)
+    """Proximity sensor LED duty ratio setting"""
+
     # ALS_Data_LM - ALS output data
     light = ROUnaryStruct(0x09, "<H")
     """Ambient light data"""
 
     # ALS_CONF - ALS integration time, persistence, interrupt, function enable/disable
-    light_settings = UnaryStruct(0x00, "<H")
     light_shutdown = RWBit(0x00, 0, register_width=2)
     """Ambient light sensor shutdown. When True, ambient light data is disabled."""
+    light_integration_time = RWBits(2, 0x00, 6, register_width=2)
+    """Ambient light sensor integration time setting. Longer time has higher sensitivity."""
+    light_interrupt = RWBit(0x00, 1, register_width=2)
+    """Ambient light sensor interrupt enable. 0 when disabled, 1 when enabled."""
 
     # ALS_THDL_LM - ALS low interrupt threshold setting
     light_low_threshold = UnaryStruct(0x02, "<H")
@@ -124,5 +181,3 @@ class VCNL4040:
 
         self.proximity_shutdown = False
         self.light_shutdown = False
-        # self.proximity_smart_persistence = True
-        # self.white_enable = False
